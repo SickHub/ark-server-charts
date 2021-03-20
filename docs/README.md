2fa7c0d2c1b499faaff648bee74d9f0ad0c174f0
DrPsychick <drpsychick@drsick.net>
2021-03-20 20:41:47 +0100

    publish chart

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,5 @@
+
+
 ```
 helm repo add drpsychick https://drpsychick.github.io/ark-cluster-chart
 helm search repo drpsychick
--- publish-chart.sh
+++ publish-chart.sh
@@ -6,3 +6,7 @@ helm package ark-cluster
 
 mv ./charts/ark-cluster-*.tgz ./docs/
 helm repo index ./docs --url https://drpsychick.github.io/ark-cluster-chart/
+
+git add docs
+git commit -m 'publish chart' -av
+git push
